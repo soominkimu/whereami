267d4182c706cf34f885b75bc7bdfbb2d91bd58e
Soomin Kimu <soominkimu@gmail.com>
2018-12-22 20:29:14 +0900

    Promise version

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+// WhereAmI
 import React from 'react';
 import './App.css';
 
@@ -5,38 +6,40 @@ import './App.css';
 
 const urlIpLookUp = 'http://ip-api.com/json?lang=ja';
 
-const requestUrlJson = (url, onSuccess, onError) => {
-  fetch(url)
-    .then(res => {
-      let ct = res.headers.get("content-type");
-      if (ct && ct.includes("application/json"))
-        return res.json();
-      throw new TypeError("Oops, no JSON data!");
-    })
-    .then(onSuccess)  // callback on Success
-    .catch(onError);  // callback on Error
-}
+const requestUrlJson = (url) =>
+  new Promise((resolve, reject) => {  // wrapper (using Promise instead of a callback function)
+    fetch(url)
+      .then(res => {
+        let ct = res.headers.get("content-type");
+        if (ct && ct.includes("application/json"))
+          return res.json();
+        throw new TypeError("Oops, no JSON data!");
+      })
+      .then(resolve)
+      .catch(reject);
+  });
 
-const requestCurrentUserPosition = (onSuccess, onError) => {
-  if ("geolocation" in navigator) {
-    navigator.geolocation.getCurrentPosition( // async
-      // 1. success: a callback function that takes a Position object
-      // 2. error  : an optional callback function that takes a PositionError object
-      // 3. option : an optional PositionOptions object
-      onSuccess,
-      onError,
-      {
-        enableHighAccuracy: true,
-        timeOut: 5000,  // amount of time before the error callback is invoke
-        maximumAge: 0   // maximum cached position age
-      }
-    );
-  } else {
-    const errMsg = 'geolocation is not enabled on this browser';
-    onError(errMsg);
-    console.log(errMsg);
-  }
-}
+const requestCurrentUserPosition = () => 
+  new Promise((resolve, reject) => {  // wrapper
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition( // async
+        // 1. success: a callback function that takes a Position object
+        // 2. error  : an optional callback function that takes a PositionError object
+        // 3. option : an optional PositionOptions object
+        resolve,
+        reject,
+        {
+          enableHighAccuracy: true,
+          timeOut: 5000,  // amount of time before the error callback is invoke
+          maximumAge: 0   // maximum cached position age
+        }
+      );
+    } else {
+      const errMsg = 'geolocation is not enabled on this browser';
+      reject(errMsg);
+      console.log(errMsg);
+    }
+  });
 
 class App extends React.Component {
   constructor(props) {
@@ -52,7 +55,10 @@ class App extends React.Component {
     console.log(navigator.userAgent, navigator.vendor);
     this.setState({ isLoading: true });
     
-    requestUrlJson(urlIpLookUp,
+    const promiseIp  = requestUrlJson(urlIpLookUp);
+    const promisePos = requestCurrentUserPosition();
+
+    promiseIp.then(
       data => {
         this.setState({ data, isLoading: false });
         console.log(JSON.stringify(data));
@@ -60,7 +66,7 @@ class App extends React.Component {
       error => this.setState({ error, isLoading: false })
     );
 
-    requestCurrentUserPosition(
+    promisePos.then(
       pos => {
         this.setState({ pos })
         console.log(pos.coords.latitude, pos.coords.longitude);
@@ -70,8 +76,8 @@ class App extends React.Component {
   }
 
   render() {
-
     const { pos, data, isLoading, error } = this.state;
+
     if (!data)     return <p>Please wait ...</p>;
     if (error)     return <p>{error.message}</p>;
     if (isLoading) return <p>Loading ...</p>;
@@ -82,10 +88,8 @@ class App extends React.Component {
         <li>Country:{data.country}</li>
         <li>Latitude, Longitude:{data.lat},{data.lon}</li>
         <li>IP:{data.query}</li>
-        <li>Geolocation Position:
-          {pos && pos.coords.latitude},
-          {pos && pos.coords.longitude},
-          {pos && pos.coords.accuracy}</li>
+        <li>Geolocation Position: {pos ?
+            `${pos.coords.latitude},${pos.coords.longitude},${pos.coords.accuracy}` : '🕛'}</li>
       </div>
     );
   }
